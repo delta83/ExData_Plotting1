cef9bf40da5b1428b815cc839785e6e4cb87066d
Sharad Agarwal 1 <sharad.ar@mt-c17n57f2g3qj.fkipl.flipkart.com>
2016-08-03 17:30:04 +0530

    Assignment 1

--- plot1.R
+++ plot1.R
@@ -1,11 +1,6 @@
-plot1 <- function() {
-  inp_table <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", colClasses = "character")
-  inp_table$Date <- as.Date(inp_table$Date, format = "%d/%m/%Y")
-  working_table <- inp_table[inp_table$Date == "2007-02-01" | inp_table$Date == "2007-02-02", ]
-  active_power <- as.numeric(working_table$Global_active_power)
-  max_active_power <- max(active_power, na.rm = TRUE)
-  num_bars <- max_active_power/0.5
-  png(filename = "plot1.png", width = 480, height = 480)
-  hist(active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", breaks = num_bars, col = "red")
-  dev.off()
-}
+file <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+new_file <- file[which(file$Date == '1/2/2007' | file$Date == '2/2/2007'), ]
+new_file$Date <- as.Date(new_file$Date, '%d/%m/%Y')
+png(filename = "plot1.png", width = 480, height = 480)
+hist(as.numeric(new_file$Global_active_power), col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power")
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,15 +1,10 @@
-plot3 <- function() {
-  inp_table <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", colClasses = "character")
-  inp_table$Time <- strptime(paste(inp_table$Date, inp_table$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
-  inp_table$Date <- as.Date(inp_table$Date, format = "%d/%m/%Y")
-  working_table <- inp_table[inp_table$Date == "2007-02-01" | inp_table$Date == "2007-02-02", ]
-  sub_metering_1 <- as.numeric(working_table$Sub_metering_1)
-  sub_metering_2 <- as.numeric(working_table$Sub_metering_2)
-  sub_metering_3 <- as.numeric(working_table$Sub_metering_3)
-  png(filename = "plot3.png", width = 480, height = 480)
-  plot(working_table$Time, sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "", col = "black")
-  lines(working_table$Time, sub_metering_2, col = "red")
-  lines(working_table$Time, sub_metering_3, col = "blue")
-  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), cex = 0.5, lty = c(1,1))
-  dev.off()
-}
+file <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+new_file <- file[which(file$Date == '1/2/2007' | file$Date == '2/2/2007'), ]
+new_file['DateTime'] <- paste(new_file$Date, new_file$Time, sep = " ")
+new_file$DateTime <- strptime(new_file$DateTime, format = "%d/%m/%Y %H:%M:%S")
+png(filename = "plot3.png", width = 480, height = 480)
+plot(new_file$DateTime, new_file$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+points(new_file$DateTime, new_file$Sub_metering_2, type = "l", col = "red")
+points(new_file$DateTime, new_file$Sub_metering_3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), pch = 46, lwd = 1)
+dev.off()
